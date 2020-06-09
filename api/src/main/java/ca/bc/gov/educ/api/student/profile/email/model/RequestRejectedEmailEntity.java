9b3e77238453bd684281e078d9c9abd49b4dcaf8
mishraomp <omprakashmishra3978@gmail.com>
2020-06-09 16:23:56 -0700

    adding object construction..

--- api/src/main/java/ca/bc/gov/educ/api/student/profile/email/model/RequestRejectedEmailEntity.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/profile/email/model/RequestRejectedEmailEntity.java
@@ -1,14 +1,18 @@
 package ca.bc.gov.educ.api.student.profile.email.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class RequestRejectedEmailEntity extends BaseEmailEntity {
 
   @NotNull(message = "Rejection Reason can not be null")
