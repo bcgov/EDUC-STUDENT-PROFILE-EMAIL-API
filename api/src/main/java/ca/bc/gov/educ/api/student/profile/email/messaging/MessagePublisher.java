c8e3039c921c5f3d9e8518f8bc8b4e19d8d297af
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2020-11-02 13:38:52 -0800

    sonar fixes...

--- api/src/main/java/ca/bc/gov/educ/api/student/profile/email/messaging/MessagePublisher.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/profile/email/messaging/MessagePublisher.java
@@ -1,40 +1,24 @@
 package ca.bc.gov.educ.api.student.profile.email.messaging;
 
 import ca.bc.gov.educ.api.student.profile.email.props.ApplicationProperties;
-import ca.bc.gov.educ.api.student.profile.email.service.EventHandlerService;
 import io.nats.streaming.AckHandler;
 import io.nats.streaming.Options;
-import io.nats.streaming.StreamingConnection;
 import io.nats.streaming.StreamingConnectionFactory;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.Closeable;
 import java.io.IOException;
 import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-
-import static lombok.AccessLevel.PRIVATE;
 
 @Component
 @Slf4j
 @SuppressWarnings("java:S2142")
-public class MessagePublisher implements Closeable {
-  private final ExecutorService executorService = Executors.newFixedThreadPool(2);
-  private StreamingConnection connection;
-  private final StreamingConnectionFactory connectionFactory;
-
-  @Getter(PRIVATE)
-  private final EventHandlerService eventHandlerService;
+public class MessagePublisher extends MessagePubSub {
 
   @Autowired
-  public MessagePublisher(final ApplicationProperties applicationProperties, final EventHandlerService eventHandlerService) throws IOException, InterruptedException {
-    this.eventHandlerService = eventHandlerService;
+  public MessagePublisher(final ApplicationProperties applicationProperties) throws IOException, InterruptedException {
     Options options = new Options.Builder()
             .natsUrl(applicationProperties.getNatsUrl())
             .clusterId(applicationProperties.getNatsClusterId())
@@ -80,46 +64,4 @@ public class MessagePublisher implements Closeable {
     connection.publish(subject, message, getAckHandler());
   }
 
-  /**
-   * This method will keep retrying for a connection.
-   */
-  private void connectionLostHandler(StreamingConnection streamingConnection, Exception e) {
-    if (e != null) {
-      int numOfRetries = 1;
-      while (true) {
-        try {
-          log.trace("retrying connection as connection was lost :: retrying ::" + numOfRetries++);
-          connection = connectionFactory.createConnection();
-          log.info("successfully reconnected after {} attempts", numOfRetries);
-          break;
-        } catch (IOException | InterruptedException ex) {
-          log.error("exception occurred", ex);
-          try {
-            double sleepTime = (2 * numOfRetries);
-            TimeUnit.SECONDS.sleep((long) sleepTime);
-          } catch (InterruptedException exc) {
-            log.error("exception occurred", exc);
-          }
-
-        }
-      }
-    }
-  }
-
-
-  @Override
-  public void close() {
-    if (!executorService.isShutdown()) {
-      executorService.shutdown();
-    }
-    if(connection != null){
-      log.info("closing nats connection in the publisher...");
-      try {
-        connection.close();
-      } catch (IOException | TimeoutException | InterruptedException e) {
-        log.error("error while closing nats connection in the publisher...", e);
-      }
-      log.info("nats connection closed in the publisher...");
-    }
-  }
 }
--- api/src/main/java/ca/bc/gov/educ/api/student/profile/email/messaging/MessageSubscriber.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/profile/email/messaging/MessageSubscriber.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.io.Closeable;
 import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -35,12 +34,10 @@ import static lombok.AccessLevel.PRIVATE;
 @Component
 @Slf4j
 @SuppressWarnings("squid:S2142")
-public class MessageSubscriber implements Closeable {
+public class MessageSubscriber extends MessagePubSub {
 
   @Getter(PRIVATE)
   private final EventHandlerService eventHandlerService;
-  private StreamingConnection connection;
-  private final StreamingConnectionFactory connectionFactory;
 
   @Autowired
   public MessageSubscriber(final ApplicationProperties applicationProperties, final EventHandlerService eventHandlerService) throws IOException, InterruptedException {
@@ -81,14 +78,19 @@ public class MessageSubscriber implements Closeable {
 
   /**
    * This method will keep retrying for a connection.
+   *
+   * @param streamingConnection the streaming connection
+   * @param e                   the e
+   * @return the int
    */
-
-  private void connectionLostHandler(StreamingConnection streamingConnection, Exception e) {
+  @Override
+  protected int connectionLostHandler(StreamingConnection streamingConnection, Exception e) {
+    int numOfRetries = 1;
     if (e != null) {
-      int numOfRetries = 1;
-      numOfRetries = retryConnection(numOfRetries);
+      numOfRetries = super.connectionLostHandler(streamingConnection, e);
       retrySubscription(numOfRetries);
     }
+    return numOfRetries;
   }
 
   private void retrySubscription(int numOfRetries) {
@@ -110,36 +112,4 @@ public class MessageSubscriber implements Closeable {
     }
   }
 
-  private int retryConnection(int numOfRetries) {
-    while (true) {
-      try {
-        log.trace("retrying connection as connection was lost :: retrying ::" + numOfRetries++);
-        connection = connectionFactory.createConnection();
-        log.info("successfully reconnected after {} attempts", numOfRetries);
-        break;
-      } catch (IOException | InterruptedException ex) {
-        log.error("exception occurred", ex);
-        try {
-          double sleepTime = (2 * numOfRetries);
-          TimeUnit.SECONDS.sleep((long) sleepTime);
-        } catch (InterruptedException exc) {
-          log.error("exception occurred", exc);
-        }
-      }
-    }
-    return numOfRetries;
-  }
-
-  @Override
-  public void close() {
-    if(connection != null){
-      log.info("closing nats connection in the subscriber...");
-      try {
-        connection.close();
-      } catch (IOException | TimeoutException | InterruptedException e) {
-        log.error("error while closing nats connection in the subscriber...", e);
-      }
-      log.info("nats connection closed in the subscriber...");
-    }
-  }
 }
