9b55291995399545c5c1063e81acb25b698211d4
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2020-11-02 15:21:23 -0800

    fixing code smells...

--- api/src/test/java/ca/bc/gov/educ/api/student/profile/email/validator/EmailValidatorTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/student/profile/email/validator/EmailValidatorTest.java
@@ -1,70 +1,31 @@
 package ca.bc.gov.educ.api.student.profile.email.validator;
 
 import ca.bc.gov.educ.api.student.profile.email.model.UMPRequestEmailVerificationEntity;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import lombok.val;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class EmailValidatorTest {
   private final EmailValidator validator = new EmailValidator();
 
   @Test
-  public void testValidateEmail_GivenInvalidEmail_ShouldAddAnErrorToTheList() {
-    UMPRequestEmailVerificationEntity entity = new UMPRequestEmailVerificationEntity("123", "BCSC","","");
-    entity.setEmailAddress("a@b.c");
+  @Parameters({
+      "a@b.c, 1",
+      ".username@yahoo.com,1",
+      "username@yahoo.com.,1",
+      "username@yahoo..com,1",
+      "1@2.3,1",
+      "1@gov.bc.ca,0",
+      "username@test.ca,0",
+      "username@yahoo.com,0",
+  })
+  public void testValidateEmail_givenDifferentInputs_shouldReturnExpectedResult(String email, int expectedErrorSize) {
+    UMPRequestEmailVerificationEntity entity = new UMPRequestEmailVerificationEntity("123", "BCSC", "", "");
+    entity.setEmailAddress(email);
     val result = validator.validateEmail(entity);
-    assert (result.size() == 1);
-  }
-
-  @Test
-  public void testValidateEmail1_GivenInvalidEmail_ShouldAddAnErrorToTheList() {
-    UMPRequestEmailVerificationEntity entity = new UMPRequestEmailVerificationEntity("123", "BCSC","","");
-    entity.setEmailAddress(".username@yahoo.com");
-    val result = validator.validateEmail(entity);
-    assert (result.size() == 1);
-  }
-
-  @Test
-  public void testValidateEmail2_GivenInvalidEmail_ShouldAddAnErrorToTheList() {
-    UMPRequestEmailVerificationEntity entity = new UMPRequestEmailVerificationEntity("123", "BCSC","","");
-    entity.setEmailAddress("username@yahoo.com.");
-    val result = validator.validateEmail(entity);
-    assert (result.size() == 1);
-  }
-  @Test
-  public void testValidateEmail2_GivenValidEmail_ShouldNotAddAnErrorToTheList() {
-    UMPRequestEmailVerificationEntity entity = new UMPRequestEmailVerificationEntity("123", "BCSC","","");
-    entity.setEmailAddress("username@yahoo.com");
-    val result = validator.validateEmail(entity);
-    assert (result.size() == 0);
-  }
-  @Test
-  public void testValidateEmail3_GivenValidEmail_ShouldNotAddAnErrorToTheList() {
-    UMPRequestEmailVerificationEntity entity = new UMPRequestEmailVerificationEntity("123", "BCSC","","");
-    entity.setEmailAddress("username@test.ca");
-    val result = validator.validateEmail(entity);
-    assert (result.size() == 0);
-  }
-  @Test
-  public void testValidateEmail3_GivenInValidEmail_ShouldNotAddAnErrorToTheList() {
-    UMPRequestEmailVerificationEntity entity = new UMPRequestEmailVerificationEntity("123", "BCSC","","");
-    entity.setEmailAddress("username@yahoo..com");
-    val result = validator.validateEmail(entity);
-    assert (result.size() == 1);
-  }
-
-  @Test
-  public void testValidateEmail4_GivenInValidEmail_ShouldNotAddAnErrorToTheList() {
-    UMPRequestEmailVerificationEntity entity = new UMPRequestEmailVerificationEntity("123", "BCSC","","");
-    entity.setEmailAddress("1@2.3");
-    val result = validator.validateEmail(entity);
-    assert (result.size() == 1);
-  }
-
-  @Test
-  public void testValidateEmail5_GivenValidEmail_ShouldNotAddAnErrorToTheList() {
-    UMPRequestEmailVerificationEntity entity = new UMPRequestEmailVerificationEntity("123", "BCSC","","");
-    entity.setEmailAddress("1@gov.bc.ca");
-    val result = validator.validateEmail(entity);
-    assert (result.size() == 0);
+    assert (result.size() == expectedErrorSize);
   }
 }
