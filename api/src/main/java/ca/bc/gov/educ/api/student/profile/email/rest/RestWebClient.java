4157c1b3254307b4c57f410e8677a6a12e4569f8
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-05-25 19:53:22 -0700

    logger updates...

--- api/src/main/java/ca/bc/gov/educ/api/student/profile/email/rest/RestWebClient.java
+++ api/src/main/java/ca/bc/gov/educ/api/student/profile/email/rest/RestWebClient.java
@@ -2,9 +2,12 @@ package ca.bc.gov.educ.api.student.profile.email.rest;
 
 import ca.bc.gov.educ.api.student.profile.email.props.ApplicationProperties;
 import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.InMemoryReactiveOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -13,6 +16,7 @@ import org.springframework.security.oauth2.client.web.reactive.function.client.S
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.DefaultUriBuilderFactory;
+import reactor.netty.http.client.HttpClient;
 
 /**
  * The type Rest web client.
@@ -20,7 +24,8 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 @Configuration
 @Profile("!test")
 public class RestWebClient {
-
+  private final DefaultUriBuilderFactory factory;
+  private final ClientHttpConnector connector;
   /**
    * The Props.
    */
@@ -33,28 +38,37 @@ public class RestWebClient {
    */
   public RestWebClient(final ApplicationProperties props) {
     this.props = props;
+    this.factory = new DefaultUriBuilderFactory();
+    this.factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
+    final HttpClient client = HttpClient.create().compress(true);
+    client.warmup()
+      .block();
+    this.connector = new ReactorClientHttpConnector(client);
   }
 
 
   @Bean
-  WebClient chesWebClient() {
+  @Autowired
+  WebClient webClient(final WebClient.Builder builder) {
     val clientRegistryRepo = new InMemoryReactiveClientRegistrationRepository(ClientRegistration
-        .withRegistrationId(this.props.getChesClientID())
-        .tokenUri(this.props.getChesTokenURL())
-        .clientId(this.props.getChesClientID())
-        .clientSecret(this.props.getChesClientSecret())
-        .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-        .build());
+      .withRegistrationId(this.props.getChesClientID())
+      .tokenUri(this.props.getChesTokenURL())
+      .clientId(this.props.getChesClientID())
+      .clientSecret(this.props.getChesClientSecret())
+      .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+      .build());
     val clientService = new InMemoryReactiveOAuth2AuthorizedClientService(clientRegistryRepo);
     val authorizedClientManager =
-        new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(clientRegistryRepo, clientService);
+      new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(clientRegistryRepo, clientService);
     val oauthFilter = new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
     oauthFilter.setDefaultClientRegistrationId(this.props.getChesClientID());
-    final DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory();
-    factory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
-    return WebClient.builder()
-        .uriBuilderFactory(factory)
-        .filter(oauthFilter)
-        .build();
+    return builder
+      .codecs(configurer -> configurer
+        .defaultCodecs()
+        .maxInMemorySize(100 * 1024 * 1024))
+      .clientConnector(this.connector)
+      .uriBuilderFactory(this.factory)
+      .filter(oauthFilter)
+      .build();
   }
 }
